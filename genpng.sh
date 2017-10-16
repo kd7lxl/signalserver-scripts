b26c8e53c88c0c1752714c90e09a3770c2550139
Tom Hayward <tom@tomh.us>
2017-10-15 20:27:26 -0700

    add genkmz.sh and genpng.sh

---
+++ genpng.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+SRTMDIR=/raid/share/Software/Mapping\&GPS/SRTM/splat
+
+if [[ $# -eq 0 ]] ; then
+	echo Usage:
+	echo $0 -lat 46.4879 -lon -123.2144 -txh 60 -f 145 -erp 30 -rxh 5 -rt -100 -R 80 -o outfile | ./genkmz.sh
+	echo The last argument must be the output name.
+	exit 1
+fi
+
+for name; do true; done
+
+#time ./signalserverHD -sdf $SRTMDIR/SRTM1 -dbm -pm 1 -dbg $@ 2>&1
+time ./signalserver -sdf $SRTMDIR/SRTM3 -dbm -pm 1 -dbg $@ 2>&1
+# to resize, add: -resize 7000x7000\>
+convert $name.ppm -transparent white $name.png
